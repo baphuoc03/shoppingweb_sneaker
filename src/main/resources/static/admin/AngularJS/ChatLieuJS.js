0f211ac7ab8126e651759aeed47db5cbf2d0aead
Nguyễn Bá Phước <baphuoc@MacBook-4.local>
2023-10-11 18:38:47 +0700

    quan ly size, chat lieu

---
+++ src/main/resources/static/admin/AngularJS/ChatLieuJS.js
@@ -0,0 +1,59 @@
+var app = angular.module("chat-lieu", [])
+app.controller("chat-lieu-ctrl", function ($scope, $http){
+    $scope.form = {}
+    $scope.items = []
+    $scope.findALL = function (){
+        $http.get("/admin/chat-lieu/find-all").then(resp => {
+            console.log(resp.data)
+            $scope.items = resp.data;
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+    $scope.findALL()
+
+    $scope.create = function (){
+        var chatLieu = {
+            id: $scope.id,
+            ten: $scope.ten,
+            ngayTao: $scope.ngayTao,
+            ngayCapNhat: $scope.ngayCapNhat
+        }
+        $http.post("http://localhost:8080/admin/chat-lieu/add", chatLieu).then(r => {
+            location.reload();
+            alert("Thêm Thành Công")
+        })
+    }
+    $scope.delete = function (id){
+        var url = "/admin/chat-lieu/delete" + "/" + id;
+        $http.delete(url).then(function (r){
+            alert("Xóa Thành Công")
+            location.reload();
+        })
+    }
+    $scope.getChatLieu = function (id){
+        var url = "/admin/chat-lieu/chi-tiet" + "/" + id;
+        $http.get(url).then(function (r){
+
+            let chatLieu = r.data;
+            $scope.id = chatLieu.id;
+            $scope.ten = chatLieu.ten;
+            $scope.ngayTao = chatLieu.ngayTao;
+            $scope.ngayCapNhat = chatLieu.ngayCapNhat;
+        })
+    }
+
+    $scope.update = function (id){
+        var url = "/admin/chat-lieu/update" + "/" + id;
+        var updateChatLieu = {
+            id: $scope.id,
+            ten: $scope.ten,
+            ngayTao: $scope.ngayTao,
+            ngayCapNhat: $scope.ngayCapNhat,
+        }
+        $http.put(url, updateChatLieu).then(function (r){
+            location.reload();
+            alert("Update Thành công")
+        })
+    }
+})
---
+++ src/main/resources/static/admin/AngularJS/SizeJS.js
@@ -0,0 +1,61 @@
+var app = angular.module("size", [])
+app.controller("size-ctrl" ,function ($scope, $http){
+    $scope.form = {}
+    $scope.items = []
+    $scope.findAll = function (){
+        $http.get("/admin/size/find-all").then(resp => {
+            console.log(resp.data)
+            $scope.items = resp.data;
+        }).catch(error =>{
+            console.log(error)
+        })
+    }
+        $scope.findAll()
+
+    $scope.create = function (){
+        var size = {
+            ma: $scope.ma,
+            chieuDai: $scope.chieuDai,
+            ngayTao: $scope.ngayTao,
+            ngayCapNhat: $scope.ngayCapNhat
+        }
+        $http.post("http://localhost:8080/admin/size/add", size).then(r => {
+            location.reload();
+            alert("Thêm thành công")
+        })
+    }
+
+    $scope.getSize = function (ma){
+        var url ="/admin/size/chi-tiet" + "/" + ma;
+        console.log(url)
+        $http.get(url).then(function (r){
+            console.log(r.data)
+            let size = r.data;
+            $scope.ma = size.ma;
+            $scope.chieuDai = size.chieuDai;
+            $scope.ngayTao = size.ngayTao;
+            $scope.ngayCapNhat = size.ngayCapNhat;
+        })
+    }
+   $scope.delete = function (ma){
+        var url ="/admin/size/delete" + "/" + ma;
+        $http.delete(url).then(function (r){
+            alert("Xóa Thành công")
+            location.reload();
+        })
+   }
+   $scope.update = function (ma){
+        var url = "/admin/size/update" + "/" +ma;
+        var updateSize = {
+            ma: $scope.ma,
+            chieuDai: $scope.chieuDai,
+            ngayTao: $scope.ngayTao,
+            ngayCapNhat: $scope.ngayCapNhat,
+        }
+        $http.put(url, updateSize).then(function (r){
+            location.reload();
+            alert("Update Thành công")
+        })
+   }
+})
+
